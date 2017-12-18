6aa95acf7b238518d9dae9398ffb8a3450706f03
Rich T <rtranter25@gmail.com>
2017-12-18 11:40:09 -0600

    adding all fileles

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('McDonalds');
+INSERT INTO burgers (burger_name) VALUES ('Burger King');
+INSERT INTO burgers (burger_name) VALUES ('Culvers');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wheezer', 'true');
